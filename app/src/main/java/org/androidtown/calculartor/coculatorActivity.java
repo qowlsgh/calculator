da15ba5631ba01d282fb096cf79a3030da01b0de
ghwlsqo819 <ghwlsqo819@gmail.com>
2017-03-24 01:58:55 +0900

    first commit

---
+++ app/src/main/java/org/androidtown/calculartor/coculatorActivity.java
@@ -0,0 +1,13 @@
+package org.androidtown.calculartor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class coculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coculator);
+    }
+}
